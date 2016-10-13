fb0ec799b6e663477da07a0a16d73f355386b818
Ogaga Uzoh <friendsofglory@gmail.com>
2016-10-12 22:47:45 -0400

    Create DetermineJobsRunning.sh

---
+++ DetermineJobsRunning.sh
@@ -0,0 +1,6 @@
+ps -ef | grep DSD.RUN
+#format UID       PID   PPID    C STIME TTY TIME     CMD 
+######  dsadm     31051 31045   0 Oct11 ?   00:00:01 phantom DSD.RUN SAVINGS_MVPCARD_MSTR_SJ. 0/50/0/0/0/0/0/
+######pid is the most important
+
+###kill -9 pid
